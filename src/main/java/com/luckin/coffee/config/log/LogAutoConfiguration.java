092c6750787ad8409f12c8f2ff81c713491e5f56
di.zhang <1>
2022-09-15 19:58:28 +0800

    初始化

---
+++ src/main/java/com/luckin/coffee/config/log/LogAutoConfiguration.java
@@ -0,0 +1,15 @@
+package com.luckin.coffee.config.log;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogAutoConfiguration {
+
+    //启动 监控函数
+
+    //注册 销毁函数
+
+
+
+}
